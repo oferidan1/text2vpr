d71e931ee2ac64e6cae76d5e71bfcd726ae9b145
oferidan1 <oferidan1@gmail.com>
2025-10-26 00:35:34 +0300

    s_ij

--- MixVPR_text/main.py
+++ MixVPR_text/main.py
@@ -16,8 +16,10 @@ from mixvpr_text import VPRModel_text
 def parse_arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # Resume parameters
-    parser.add_argument("--resume_model", type=str, default='checkpoints/resnet50_MixVPR_4096_channels(1024)_rows(4).ckpt', help="path to model to resume, e.g. logs/.../best_model.pth")
-    # Other parameters    
+    #parser.add_argument("--vpr_resume_model", type=str, default='checkpoints/resnet50_MixVPR_4096_channels(1024)_rows(4).ckpt', help="path to model to resume, e.g. logs/.../best_model.pth")    
+    parser.add_argument("--vpr_resume_model", type=str, default='checkpoints/resnet50_MixVPR_512_channels(256)_rows(2).ckpt', help="path to model to resume, e.g. logs/.../best_model.pth")
+    parser.add_argument("--vpr_rows", type=int, default=2, help="number of rows for vpr embeddings")
+    # Other parameters
     parser.add_argument("--gpu", type=str, default='0', help="gpu id(s) to use")
     parser.add_argument("--vpr_dim", type=int, default=512, help="dimension of the vpr embeddings")
     parser.add_argument("--train_csv", type=str, default="/mnt/d/data/gsv_cities/gsv_cities_predictions.csv")    
@@ -72,10 +74,10 @@ if __name__ == '__main__':
         agg_config={'in_channels' : 1024,
                 'in_h' : 20,
                 'in_w' : 20,
-                'out_channels' : args.vpr_dim // 4, # final out dim will be out_channels * out_rows
+                'out_channels' : args.vpr_dim // args.vpr_rows, # final out dim will be out_channels * out_rows
                 'mix_depth' : 4,
                 'mlp_ratio' : 1,
-                'out_rows' : 4}, # the output dim will be (out_rows * out_channels)
+                'out_rows' : args.vpr_rows}, # the output dim will be (out_rows * out_channels)
         
         #---- Train hyperparameters
         lr=0.05, # 0.0002 for adam, 0.05 or sgd (needs to change according to batch size)
@@ -103,8 +105,8 @@ if __name__ == '__main__':
         is_trainable_text_encoder=args.is_trainable_text_encoder,
     )
     
-    if args.is_encode_image and  args.resume_model is not None:
-        model_state_dict = torch.load(args.resume_model)
+    if args.is_encode_image and  args.vpr_resume_model is not None:
+        model_state_dict = torch.load(args.vpr_resume_model)
         model.vpr_encoder.load_state_dict(model_state_dict)
         
     model = model.to('cuda')
