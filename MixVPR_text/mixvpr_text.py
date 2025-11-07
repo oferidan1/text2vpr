fixed weighting

MixVPR_text/mixvpr_text.py
@@ -11,31 +11,8 @@ from peft import LoraConfig, get_peft_model, TaskType
 from transformers import AutoModel
 from transformers import AutoTokenizer, AutoModel
 import numpy as np
+import torchvision.transforms as transforms
 
-
-
-# class GeM(nn.Module):
-#     def __init__(self, p=3, eps=1e-6, work_with_tokens=False):
-#         super().__init__()
-#         self.p = Parameter(torch.ones(1)*p)
-#         self.eps = eps
-#         self.work_with_tokens=work_with_tokens
-#     def forward(self, x):
-#         return gem(x, p=self.p, eps=self.eps, work_with_tokens=self.work_with_tokens)
-#     def __repr__(self):
-#         return self.__class__.__name__ + '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + ', ' + 'eps=' + str(self.eps) + ')'
-
-# def gem(x, p=3, eps=1e-6, work_with_tokens=False):
-#     if work_with_tokens:
-#         x = x.permute(0, 2, 1)
-#         # unseqeeze to maintain compatibility with Flatten
-#         return F.avg_pool1d(x.clamp(min=eps).pow(p), (x.size(-1))).pow(1./p).unsqueeze(3)
-#     else:
-#         return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
-
-# class Flatten(nn.Module):
-#     def __init__(self): super().__init__()
-#     def forward(self, x): assert x.shape[2] == x.shape[3] == 1; return x[:,:,0,0]
 
 class GeMPooling(nn.Module):
     """
@@ -89,12 +66,6 @@ class GeMPooling(nn.Module):
                '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + \
                ', ' + 'eps=' + str(self.eps) + ')'
 
-class L2Norm(nn.Module):
-    def __init__(self, dim=1):
-        super().__init__()
-        self.dim = dim
-    def forward(self, x):
-        return F.normalize(x, p=2, dim=self.dim)
 
 class VPRModel_text(pl.LightningModule):
     """This is the main model for Visual Place Recognition
@@ -130,13 +101,13 @@ class VPRModel_text(pl.LightningModule):
                 miner_margin=0.1,
                 faiss_gpu=False,
                 text_encoder_name='BAAI/bge-large-en-v1.5',
-                embeds_dim=1024,
+                embeds_dim=512,
                 is_freeze_vpr=True,
                 is_freeze_text=True,
                 fusion_type='mlp',
-                is_encode_image=False,
+                is_encode_image=True,
                 is_encode_text=True,
-                is_trainable_text_encoder=True,
+                is_trainable_text_encoder=False,
                  ):
         super().__init__()
         
@@ -173,16 +144,17 @@ class VPRModel_text(pl.LightningModule):
         self.my_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         if is_encode_image:
-            self.vpr_encoder = VPRModel(backbone_arch, pretrained, layers_to_freeze, layers_to_crop, agg_arch, agg_config, lr, optimizer, weight_decay, momentum, warmpup_steps, milestones, lr_mult, loss_name, miner_name, miner_margin, faiss_gpu)
+            self.vpr_encoder = VPRModel(backbone_arch, pretrained, layers_to_freeze, layers_to_crop, agg_arch, agg_config)
             vpr_output_dim = agg_config['out_rows'] * agg_config['out_channels']
             if is_freeze_vpr:
                 # Freeze vpr encoder parameters
                 for param in self.vpr_encoder.parameters():
                     param.requires_grad = False
         if is_encode_text:
-            # self.text_encoder = SentenceTransformer(text_encoder_name)
-            self.tokenizer = AutoTokenizer.from_pretrained(text_encoder_name)  
-            self.text_encoder = AutoModel.from_pretrained(text_encoder_name)
+            self.text_encoder = SentenceTransformer(text_encoder_name)
+            # self.tokenizer = AutoTokenizer.from_pretrained(text_encoder_name)  
+            # self.text_encoder = AutoModel.from_pretrained(text_encoder_name)
+        
             #self.text_aggregation = GeMPooling()
 
             if is_freeze_text:
@@ -226,6 +198,10 @@ class VPRModel_text(pl.LightningModule):
             elif self.fusion_type == 'dynamic_weighting':
                 input_dim = vpr_output_dim + text_encoder_dim
                 self.fusion = nn.Sequential(nn.Linear(input_dim, input_dim), nn.ReLU(), nn.Linear(input_dim, 2), nn.Softmax(dim=1))
+            elif self.fusion_type == 'fixed_weighting':
+                input_dim = vpr_output_dim + text_encoder_dim
+                #learn fixed parameter for weighting image and text
+                self.w_alpha = Parameter(torch.tensor([0.5]), requires_grad=True)                
         
         # Call the weight initialization function
         self.apply(self._init_weights)
@@ -243,16 +219,19 @@ class VPRModel_text(pl.LightningModule):
     def forward(self, img, text):
         w = None
         text_embeds = None
+        #img = transforms.Resize([320, 320], antialias=True)(img)
+
         if self.is_encode_image:
+            #with torch.no_grad():      
             img_embeds = self.vpr_encoder.backbone(img)
             img_embeds = self.vpr_encoder.aggregator(img_embeds)
-        if self.is_encode_text:
-            # text_embeds = self.text_encoder.encode(text, normalize_embeddings=True, convert_to_tensor=True)        
-            text_tokens = self.tokenizer(text, padding=True, truncation=True, return_tensors='pt').to(img.device)
-            model_output = self.text_encoder(**text_tokens)                        
-            text_embeds = model_output[0][:, 0]
-            text_embeds = torch.nn.functional.normalize(text_embeds, p=2, dim=1)            
-            
+        if self.is_encode_text:        
+            text_embeds = self.text_encoder.encode(text, normalize_embeddings=True, convert_to_tensor=True)        
+            # text_tokens = self.tokenizer(text, padding=True, truncation=True, return_tensors='pt').to(img.device)
+            # model_output = self.text_encoder(**text_tokens)                        
+            # text_embeds = model_output[0][:, 0]
+            # text_embeds = torch.nn.functional.normalize(text_embeds, p=2, dim=1)            
+        
             # GEM pooling            
             # model_output = self.text_encoder(**text_tokens, output_hidden_states=True, return_dict=True)
             # token_feature = model_output.last_hidden_state
@@ -280,6 +259,10 @@ class VPRModel_text(pl.LightningModule):
                 # calc dynamic weighting
                 embeds_input = torch.cat([img_embeds, text_embeds], dim=1)
                 w = self.fusion(embeds_input)                
+                embeds = img_embeds
+            elif self.fusion_type == 'fixed_weighting':
+                # use fixed weighting
+                w = self.w_alpha
                 embeds = img_embeds
                            
         elif self.is_encode_text:
@@ -504,7 +487,7 @@ class VPRModel_text(pl.LightningModule):
         print('\n\n')
 
 
-class VPRModel(pl.LightningModule):
+class VPRModel(nn.Module):
     """This is the main model for Visual Place Recognition
     we use Pytorch Lightning for modularity purposes.
 
@@ -522,21 +505,6 @@ class VPRModel(pl.LightningModule):
                 #---- Aggregator
                 agg_arch='ConvAP', #CosPlace, NetVLAD, GeM
                 agg_config={},
-                
-                #---- Train hyperparameters
-                lr=0.03, 
-                optimizer='sgd',
-                weight_decay=1e-3,
-                momentum=0.9,
-                warmpup_steps=500,
-                milestones=[5, 10, 15],
-                lr_mult=0.3,
-                
-                #----- Loss
-                loss_name='MultiSimilarityLoss', 
-                miner_name='MultiSimilarityMiner', 
-                miner_margin=0.1,
-                faiss_gpu=False
                  ):
         super().__init__()
         self.encoder_arch = backbone_arch
@@ -547,26 +515,6 @@ class VPRModel(pl.LightningModule):
         self.agg_arch = agg_arch
         self.agg_config = agg_config
 
-        self.lr = lr
-        self.optimizer = optimizer
-        self.weight_decay = weight_decay
-        self.momentum = momentum
-        self.warmpup_steps = warmpup_steps
-        self.milestones = milestones
-        self.lr_mult = lr_mult
-
-        self.loss_name = loss_name
-        self.miner_name = miner_name
-        self.miner_margin = miner_margin
-        
-        self.save_hyperparameters() # write hyperparams into a file
-        
-        self.loss_fn = utils.get_loss(loss_name)
-        self.miner = utils.get_miner(miner_name, miner_margin)
-        self.batch_acc = [] # we will keep track of the % of trivial pairs/triplets at the loss level 
-
-        self.faiss_gpu = faiss_gpu
-        
         # ----------------------------------
         # get the backbone and the aggregator
         self.backbone = helper.get_backbone(backbone_arch, pretrained, layers_to_freeze, layers_to_crop)
@@ -578,142 +526,4 @@ class VPRModel(pl.LightningModule):
         x = self.aggregator(x)
         return x
     
-    # configure the optimizer 
-    def configure_optimizers(self):
-        if self.optimizer.lower() == 'sgd':
-            optimizer = torch.optim.SGD(self.parameters(), 
-                                        lr=self.lr, 
-                                        weight_decay=self.weight_decay, 
-                                        momentum=self.momentum)
-        elif self.optimizer.lower() == 'adamw':
-            optimizer = torch.optim.AdamW(self.parameters(), 
-                                        lr=self.lr, 
-                                        weight_decay=self.weight_decay)
-        elif self.optimizer.lower() == 'adam':
-            optimizer = torch.optim.AdamW(self.parameters(), 
-                                        lr=self.lr, 
-                                        weight_decay=self.weight_decay)
-        else:
-            raise ValueError(f'Optimizer {self.optimizer} has not been added to "configure_optimizers()"')
-        scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=self.milestones, gamma=self.lr_mult)
-        return [optimizer], [scheduler]
     
-    # configure the optizer step, takes into account the warmup stage
-    def optimizer_step(self,  epoch, batch_idx,
-                        optimizer, optimizer_idx, optimizer_closure,
-                        on_tpu, using_native_amp, using_lbfgs):
-        # warm up lr
-        if self.trainer.global_step < self.warmpup_steps:
-            lr_scale = min(1., float(self.trainer.global_step + 1) / self.warmpup_steps)
-            for pg in optimizer.param_groups:
-                pg['lr'] = lr_scale * self.lr
-        optimizer.step(closure=optimizer_closure)
-        
-    #  The loss function call (this method will be called at each training iteration)
-    def loss_function(self, descriptors, labels, s_ij):
-        # we mine the pairs/triplets if there is an online mining strategy
-        if self.miner is not None:
-            miner_outputs = self.miner(descriptors, labels)
-            loss = self.loss_fn(descriptors, labels, miner_outputs)
-            
-            # calculate the % of trivial pairs/triplets 
-            # which do not contribute in the loss value
-            nb_samples = descriptors.shape[0]
-            nb_mined = len(set(miner_outputs[0].detach().cpu().numpy()))
-            batch_acc = 1.0 - (nb_mined/nb_samples)
-
-        else: # no online mining
-            loss = self.loss_fn(descriptors, labels)
-            batch_acc = 0.0
-            if type(loss) == tuple: 
-                # somes losses do the online mining inside (they don't need a miner objet), 
-                # so they return the loss and the batch accuracy
-                # for example, if you are developping a new loss function, you might be better
-                # doing the online mining strategy inside the forward function of the loss class, 
-                # and return a tuple containing the loss value and the batch_accuracy (the % of valid pairs or triplets)
-                loss, batch_acc = loss
-
-        # keep accuracy of every batch and later reset it at epoch start
-        self.batch_acc.append(batch_acc)
-        # log it
-        self.log('b_acc', sum(self.batch_acc) /
-                len(self.batch_acc), prog_bar=True, logger=True)
-        return loss
-    
-    # This is the training step that's executed at each iteration
-    def training_step(self, batch, batch_idx):
-        places, labels = batch
-        
-        # Note that GSVCities yields places (each containing N images)
-        # which means the dataloader will return a batch containing BS places
-        BS, N, ch, h, w = places.shape
-        
-        # reshape places and labels
-        images = places.view(BS*N, ch, h, w)
-        labels = labels.view(-1)
-
-        # Feed forward the batch to the model
-        descriptors = self(images) # Here we are calling the method forward that we defined above
-        loss = self.loss_function(descriptors, labels) # Call the loss_function we defined above
-        
-        self.log('loss', loss.item(), logger=True)
-        return {'loss': loss}
-    
-    # This is called at the end of eatch training epoch
-    def training_epoch_end(self, training_step_outputs):
-        # we empty the batch_acc list for next epoch
-        self.batch_acc = []
-
-    # For validation, we will also iterate step by step over the validation set
-    # this is the way Pytorch Lghtning is made. All about modularity, folks.
-    def validation_step(self, batch, batch_idx, dataloader_idx=None):
-        places, _ = batch
-        # calculate descriptors
-        descriptors = self(places)
-        return descriptors.detach().cpu()
-    
-    def validation_epoch_end(self, val_step_outputs):
-        """this return descriptors in their order
-        depending on how the validation dataset is implemented 
-        for this project (MSLS val, Pittburg val), it is always references then queries
-        [R1, R2, ..., Rn, Q1, Q2, ...]
-        """
-        dm = self.trainer.datamodule
-        # The following line is a hack: if we have only one validation set, then
-        # we need to put the outputs in a list (Pytorch Lightning does not do it presently)
-        if len(dm.val_datasets)==1: # we need to put the outputs in a list
-            val_step_outputs = [val_step_outputs]
-        
-        for i, (val_set_name, val_dataset) in enumerate(zip(dm.val_set_names, dm.val_datasets)):
-            feats = torch.concat(val_step_outputs[i], dim=0)
-            
-            if 'pitts' in val_set_name:
-                # split to ref and queries
-                num_references = val_dataset.dbStruct.numDb
-                num_queries = len(val_dataset)-num_references
-                positives = val_dataset.getPositives()
-            elif 'msls' in val_set_name:
-                # split to ref and queries
-                num_references = val_dataset.num_references
-                num_queries = len(val_dataset)-num_references
-                positives = val_dataset.pIdx
-            else:
-                print(f'Please implement validation_epoch_end for {val_set_name}')
-                raise NotImplemented
-
-            r_list = feats[ : num_references]
-            q_list = feats[num_references : ]
-            pitts_dict = utils.get_validation_recalls(r_list=r_list, 
-                                                q_list=q_list,
-                                                k_values=[1, 5, 10, 15, 20, 50, 100],
-                                                gt=positives,
-                                                print_results=True,
-                                                dataset_name=val_set_name,
-                                                faiss_gpu=self.faiss_gpu
-                                                )
-            del r_list, q_list, feats, num_references, positives
-
-            self.log(f'{val_set_name}/R1', pitts_dict[1], prog_bar=False, logger=True)
-            self.log(f'{val_set_name}/R5', pitts_dict[5], prog_bar=False, logger=True)
-            self.log(f'{val_set_name}/R10', pitts_dict[10], prog_bar=False, logger=True)
-        print('\n\n')

MixVPR_text/utils/generic_pair_loss.py
@@ -3,7 +3,7 @@ import torch
 from pytorch_metric_learning.utils import common_functions as c_f
 from pytorch_metric_learning.utils import loss_and_miner_utils as lmu
 from .base_metric_loss_function import BaseMetricLossFunction
-
+import torch.nn.functional as F
 
 class GenericPairLoss(BaseMetricLossFunction):
     def __init__(self, mat_based_loss, **kwargs):
@@ -18,30 +18,50 @@ class GenericPairLoss(BaseMetricLossFunction):
         if all(len(x) <= 1 for x in indices_tuple):
             return self.zero_losses()
         mat = self.distance(embeddings, ref_emb)
-        w_i = w[:,0].unsqueeze(1)
-        w_t = w[:,1].unsqueeze(1)   
-        # calc text sim in the batch
-        text_sim = torch.matmul(embeds2, embeds2.T)
-        img_sim = torch.matmul(embeddings, embeddings.T)
         
-        mu_text  = 0.6520
-        std_text = 0.0708
-        mu_img   = 0.0113
+        # calc text sim in the batch
+        img_sim = torch.matmul(embeddings, embeddings.T)
+        text_sim = torch.matmul(embeds2, embeds2.T)
+        # text_sim = F.cosine_similarity(embeds2, embeds2, dim=-1)
+        # img_sim = F.cosine_similarity(embeddings, embeddings, dim=-1)
+        text_sim = torch.clamp(text_sim, min=-1.0, max=1.0)
+        img_sim  = torch.clamp(img_sim, min=-1.0, max=1.0)
+                
+        # mu_text  = 0.6520
+        # std_text = 0.0708
+        mu_text  = 0.9435
+        std_text = 0.0201
+        min_text = 0.5
+        max_text = 1.0
+        mu_img   = 0.0111
         std_img  = 0.0514
+        min_img  = -0.4
+        max_img  = 1.0
         text_sim = (text_sim - mu_text) / std_text
         img_sim  = (img_sim - mu_img) / std_img
-        # w_i_ij = torch.zeros_like(img_sim)
-        # w_t_ij = torch.zeros_like(text_sim)
-        # batch_size = embeddings.shape[0]       
+        # text_sim = (text_sim - min_text) / (max_text - min_text) 
+        # img_sim  = (img_sim - min_img) / (max_img - min_img)         
+      
+        # w_i = w[:,0].unsqueeze(1)
+        # w_t = w[:,1].unsqueeze(1)
+        # w_i_ij_old = torch.zeros_like(img_sim)
+        # w_t_ij_old = torch.zeros_like(text_sim)
+        # batch_size = img_sim.shape[0]       
         # for i in range(batch_size):
         #     for j in range(batch_size):
-        #         w_i_ij[i, j] = (w_i[i] + w_i[j]) / 2.0
-        #         w_t_ij[i, j] = (w_t[i] + w_t[j]) / 2.0
+        #         w_i_ij_old[i, j] = (w_i[i] + w_i[j]) / 2.0
+        #         w_t_ij_old[i, j] = (w_t[i] + w_t[j]) / 2.0
+
+        # # calculate dynamic weights
+        w_i = w[:,0].unsqueeze(1)
+        w_t = w[:,1].unsqueeze(1)   
         w_i_ij = ((w_i.unsqueeze(1) + w_i.unsqueeze(0)) / 2.0).squeeze(-1)
         w_t_ij = ((w_t.unsqueeze(1) + w_t.unsqueeze(0)) / 2.0).squeeze(-1)
-
-        # calculate dynamic weights
         s_ij = w_i_ij * img_sim + w_t_ij * text_sim
+
+        # calculate fixed weights
+        # s_ij = w*img_sim + (1-w)*text_sim
+
         return self.loss_method(mat, indices_tuple, s_ij)
 
     def _compute_loss(self):

evaluation/calc_data_mu_std.py
@@ -10,7 +10,7 @@ from loguru import logger
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Subset
 from tqdm import tqdm
-from vlm_model import VLM_Model
+from vlm_model import VLM_Model 
 import os
 from test_dataset import TestDataset
 import visualizations
@@ -62,6 +62,10 @@ def main(args):
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
     os.environ["TOKENIZERS_PARALLELISM"] = "False"
     start_time = datetime.now()
+    
+    args.is_dual_encoder = 1
+    args.encode_mode = 'both'
+    args.dual_encoder_fusion = 'each'
 
     logger.remove()  # Remove possibly previously existing loggers
     log_dir = Path("logs") / args.log_dir / start_time.strftime("%Y-%m-%d_%H-%M-%S")
@@ -107,18 +111,30 @@ def main(args):
         
         
         sim_matrix_t = np.matmul(text_descriptors, text_descriptors.T)
-        mu_t = np.mean(sim_matrix_t)
-        std_t = np.std(sim_matrix_t)
-        logger.info(f"Text Similarity matrix mean: {mu_t:.4f}")
-        logger.info(f"Text Similarity matrix std: {std_t:.4f}")
-        
+        #calc data mu and std over text sim matrix but without diagonal elements and lower triangle
+        upper_indices = np.triu_indices_from(sim_matrix_t, k=1) 
+        sim_matrix_t_upper = sim_matrix_t[upper_indices]
+        mu_t  = np.mean(sim_matrix_t_upper)
+        std_t = np.std(sim_matrix_t_upper)
+        min_t = np.min(sim_matrix_t_upper)
+        max_t = np.max(sim_matrix_t_upper)
+        logger.info(f"Text Similarity matrix mean: {mu_t:.8f}")
+        logger.info(f"Text Similarity matrix std: {std_t:.8f}")
+        logger.info(f"Text Similarity matrix min: {min_t:.8f}")
+        logger.info(f"Text Similarity matrix max: {max_t:.8f}")
+
         sim_matrix_v = np.matmul(vision_descriptors, vision_descriptors.T)
-        mu_v = np.mean(sim_matrix_v)
-        std_v = np.std(sim_matrix_v)
-
-        logger.info(f"Vision Similarity matrix mean: {mu_v:.4f}")
-        logger.info(f"Vision Similarity matrix std: {std_v:.4f}")
-
+        #calc data mu and std over vision sim matrix but without diagonal elements and lower triangle
+        upper_indices = np.triu_indices_from(sim_matrix_v, k=1) 
+        sim_matrix_v_upper = sim_matrix_v[upper_indices]
+        mu_v  = np.mean(sim_matrix_v_upper)
+        std_v = np.std(sim_matrix_v_upper)
+        min_v = np.min(sim_matrix_v_upper)
+        max_v = np.max(sim_matrix_v_upper)
+        logger.info(f"Vision Similarity matrix mean: {mu_v:.8f}")
+        logger.info(f"Vision Similarity matrix std: {std_v:.8f}")
+        logger.info(f"Vision Similarity matrix min: {min_v:.8f}")
+        logger.info(f"Vision Similarity matrix max: {max_v:.8f}")
 
 
 if __name__ == "__main__":

evaluation/eval_vpr.py
@@ -16,6 +16,56 @@ from test_dataset import TestDataset
 import visualizations
 from math import sqrt
 
+def rerank_predictions(vision_scores, vision_predictions, text_scores, text_predictions, w_alpha, max_results):
+    # sum scores according the where vision and text predictions are the same
+    combined_scores = []
+    combined_predictions = []
+    # pre-computed mu and std for normalization over GSV
+    mu_text  = 0.9435
+    std_text = 0.0201    
+    mu_img   = 0.0111
+    std_img  = 0.0514
+    # pre-computed mu and std for normalization over amstertime 
+    # mu_text  = 0.9501
+    # std_text = 0.0189
+    # mu_img   = 0.0627
+    # std_img  = 0.0612
+    # pre-computed mu and std for normalization over nordland 
+    # mu_text  = 0.7508
+    # std_text = 0.0618
+    # mu_img   = 0.2785
+    # std_img  = 0.105
+    #clip to avoid negative scores
+    text_scores = np.clip(text_scores, a_min=-1.0, a_max=1.0)
+    vision_scores = np.clip(vision_scores, a_min=-1.0, a_max=1.0)
+    # normalize scores
+    #text_scores = (text_scores - 0.5) * 2
+
+    print("mean w_alpha vision:", w_alpha[:,0].mean(), w_alpha[:,0].std())
+    print("mean w_alpha text:", w_alpha[:,1].mean(), w_alpha[:,1].std())
+    text_scores = (text_scores - mu_text) / std_text
+    vision_scores  = (vision_scores - mu_img) / std_img
+    for v_scores, v_preds, t_scores, t_preds in zip(vision_scores, vision_predictions, text_scores, text_predictions):
+        score_dict = {}
+        for score, pred in zip(v_scores, v_preds):
+            if pred not in score_dict:
+                score_dict[pred] = 0
+            score_dict[pred] += w_alpha[pred][0] * score
+        for score, pred in zip(t_scores, t_preds):
+            if pred not in score_dict:
+                score_dict[pred] = 0            
+            score_dict[pred] += w_alpha[pred][1] * score
+        # sort by score
+        sorted_items = sorted(score_dict.items(), key=lambda x: x[1], reverse=True)
+        preds_sorted = [item[0] for item in sorted_items][:max_results]
+        scores_sorted = [item[1] for item in sorted_items][:max_results]
+        combined_predictions.append(preds_sorted)
+        combined_scores.append(scores_sorted)
+        
+    combined_predictions = np.array(combined_predictions)
+    combined_scores = np.array(combined_scores)
+    return combined_scores, combined_predictions
+
 def normlize_features(x):
     return x / np.linalg.norm(x, axis=1, keepdims=True)    
 
@@ -50,17 +100,26 @@ def encode_batch(model, args, images, texts, indices, all_descriptors, vision_de
         descriptors, text_features, w = model.encode_single(images.to(args.device), texts)
         descriptors = descriptors.cpu().numpy()
         all_descriptors[indices.numpy(), :] = descriptors
-        if args.fusion_type == 'dynamic_weighting':
+        if args.fusion_type == 'dynamic_weighting' or args.fusion_type == 'fixed_weighting':
             vision_descriptors[indices.numpy(), :] = descriptors
             text_features = text_features.cpu().numpy()
             text_descriptors[indices.numpy(), :] = text_features  
             w = w.cpu().numpy()
-            w_alpha[indices.numpy(), :] = w
+            if args.fusion_type == 'fixed_weighting':
+                #make w a 2D vector of [w, 1-w]. w in numpy
+                w = np.repeat(w, indices.shape[0], axis=0)
+                #make w a 2D vector of [w, 1-w]
+                w_alpha[indices.numpy(), :] = np.stack([w, 1-w], axis=1)                
+            else:
+                w_alpha[indices.numpy(), :] = w
             
 def get_queries_predictions(encoder_dim, database_descriptors, all_descriptors, queries_descriptors, max_results):
      # Use a kNN to find predictions
     #faiss_index = faiss.IndexFlatL2(encoder_dim)
     faiss_index = faiss.IndexFlatIP(encoder_dim)
+    #normilize descriptors for cosine similarity
+    database_descriptors = normlize_features(database_descriptors)      
+    queries_descriptors = normlize_features(queries_descriptors)
     faiss_index.add(database_descriptors)
     del database_descriptors, all_descriptors
 
@@ -68,36 +127,6 @@ def get_queries_predictions(encoder_dim, database_descriptors, all_descriptors, 
     scores, predictions = faiss_index.search(queries_descriptors, max_results)
     return scores, predictions
 
-def rerank_predictions(vision_scores, vision_predictions, text_scores, text_predictions, w_alpha, max_results):
-    # sum scores according the where vision and text predictions are the same
-    combined_scores = []
-    combined_predictions = []
-    mu_text  = 0.6520
-    std_text = 0.0708
-    mu_img   = 0.0113
-    std_img  = 0.0514
-    text_scores = (text_scores - mu_text) / std_text
-    vision_scores  = (vision_scores - mu_img) / std_img
-    for v_scores, v_preds, t_scores, t_preds in zip(vision_scores, vision_predictions, text_scores, text_predictions):
-        score_dict = {}
-        for score, pred in zip(v_scores, v_preds):
-            if pred not in score_dict:
-                score_dict[pred] = 0
-            score_dict[pred] += w_alpha[pred][0] * score
-        for score, pred in zip(t_scores, t_preds):
-            if pred not in score_dict:
-                score_dict[pred] = 0            
-            score_dict[pred] += w_alpha[pred][1] * score
-        # sort by score
-        sorted_items = sorted(score_dict.items(), key=lambda x: x[1], reverse=True)
-        preds_sorted = [item[0] for item in sorted_items][:max_results]
-        scores_sorted = [item[1] for item in sorted_items][:max_results]
-        combined_predictions.append(preds_sorted)
-        combined_scores.append(scores_sorted)
-        
-    combined_predictions = np.array(combined_predictions)
-    combined_scores = np.array(combined_scores)
-    return combined_scores, combined_predictions
 
 def main(args):
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
@@ -175,7 +204,7 @@ def main(args):
     if 1:
         # w_alpha[:,0] = alpha
         # w_alpha[:,1] = 1.0-alpha
-        if (args.is_dual_encoder and args.dual_encoder_fusion=='each') or args.fusion_type=='dynamic_weighting':        
+        if (args.is_dual_encoder and args.dual_encoder_fusion=='each') or args.fusion_type=='dynamic_weighting' or args.fusion_type=='fixed_weighting': 
             # vision
             vision_queries_descriptors = vision_descriptors[test_ds.num_database :]
             vision_database_descriptors = vision_descriptors[: test_ds.num_database]    

evaluation/vlm_model.py
@@ -48,7 +48,7 @@ class VLM_Model:
             )
             model_state_dict = torch.load(args.model_name)['state_dict']
             self.single_encoder.load_state_dict(model_state_dict)
-            self.single_encoder.eval().to(args.device)
+            self.single_encoder = self.single_encoder.eval().to(args.device)
             self.encoder_dim = self.single_encoder.embeds_dim           
         
             